2a515a5a977fa41cc331ef3d18b8d863d98c08c4
Anas <anas.mohsine@gmail.com>
2024-05-13 20:13:36 +0100

    MG-14 - Add Sum function

--- Level1.java
+++ Level1.java
@@ -2,4 +2,8 @@ public class Level1 {
     public static void main(String[] args) {
         System.out.println("Level 1!");
     }
+
+    public int sum(int a, int b) {
+        return a + b;
+    }
 }
