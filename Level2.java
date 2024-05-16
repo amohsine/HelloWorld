4cd9d252236454af7d9b30fa4b32f980fb15e261
Anas <anas.mohsine@gmail.com>
2024-05-16 11:43:14 +0100

    MG-15 - Add Level2 class with divide function

---
+++ Level2.java
@@ -0,0 +1,9 @@
+public class Level2 {
+    public static void main(String[] args) {
+        System.out.println("Level 2!");
+    }
+
+    public float divide(int a, int b) {
+        return a / b;
+    }
+}
